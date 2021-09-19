fedbd5a8e6f32c2013476ac78df28ca7ea8a7df9
Vallghall <romangusev2001@mail.ru>
2021-09-19 19:48:03 +0300

    Fix polygrammar cipher

--- internal/process/process.go
+++ internal/process/process.go
@@ -7,7 +7,8 @@ func EncodeGroup(group []rune, key int) string {
 	ks := group[0] + rune(key)
 	sb.WriteRune(ks)
 	for _, symbol := range group[1:] {
-		sb.WriteRune(symbol + ks - 'a')
+		temp := (symbol + ks - 'a' - 'a') % 26
+		sb.WriteRune(temp + 'a')
 	}
 	return sb.String()
 }
@@ -17,7 +18,11 @@ func DecodeGroup(group []rune, key int) string {
 	ks := group[0]
 	sb.WriteRune(ks - rune(key))
 	for _, symbol := range group[1:] {
-		sb.WriteRune(symbol - ks + 'a')
+		temp := symbol - ks
+		if temp < 0 {
+			temp += 26
+		}
+		sb.WriteRune(temp + 'a')
 	}
 	return sb.String()
 }
